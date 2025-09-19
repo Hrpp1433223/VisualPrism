76e7c2c5c155b2c3a4f390b9facc55f47dab1a18
Hrpp1433223 <b23090115370@cityu.edu.mo>
2025-09-19 13:23:04 +0800

    Update heatmap.sh

--- scripts/heatmap.sh
+++ scripts/heatmap.sh
@@ -1,7 +1,7 @@
-python generate_attention_heatmap.py \
-    --model_path /data/VisualPrism/checkpoints/Tokenpacker-7b \
+python visualization/generate_attention_heatmap.py \
+    --model_path ./checkpoints/VisualPrism-7b \
     --image_path /data/VisualPrism/OIP.jpg \
     --text "What's in the image?" \
-    --output_dir ./attention_heatmaps_Tokenpacker \
-    --layers 0 8 16 20 23 \
+    --output_dir ./attention_heatmaps_VisualPrism \
+    --layers 0 16 20 23 \
     --max_new_tokens 100
