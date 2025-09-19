886ef2848fdfd0617ea3e19aacb71ab07f825409
Hrpp1433223 <b23090115370@cityu.edu.mo>
2025-09-19 13:24:45 +0800

    Update visualization.sh

--- scripts/visualization.sh
+++ scripts/visualization.sh
@@ -1,7 +1,7 @@
-python run_attention_visualization.py \
-    --model_path /data/VisualPrism/checkpoints/Tokenpacker-7b \
+python visualization/run_attention_visualization.py \
+    --model_path ./checkpoints/VisualPrism-7b \
     --image_path /data/VisualPrism/OIP.jpg \
     --text "What's in the image?" \
-    --output_dir ./attention_results_Tokenpacker \
+    --output_dir ./attention_results_VisualPrism \
     --layers 0 8 16 23 \
     --max_new_tokens 100
